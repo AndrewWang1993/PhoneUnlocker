e38cc2c27c025b59450d17f4f1122e4b56266217
PeocePC <matthew98981@gmail.com>
2015-05-21 00:48:43 +0800

    add some method, right path has not implement

--- app/src/main/java/whunf/unlockscreenbypath/Unlock.java
+++ app/src/main/java/whunf/unlockscreenbypath/Unlock.java
@@ -20,9 +20,17 @@ public class Unlock extends View implements View.OnTouchListener {
     int screenHeight = 1280 - 300;
     int[] colors = {Color.LTGRAY, Color.BLUE, Color.GREEN, Color.RED};
     float[][][] dotPos = new float[3][3][3];
-    float[][][] touchedDotPos = new float[3][3][3];
+    float[][] touch2Pot = new float[2][2];
     ArrayList linePoint = new ArrayList();
     Paint paint = new Paint();
+
+    int[][] rightPath = {
+            {0, 1},
+            {1, 0},
+            {2, 1},
+            {1, 2}
+    };
+    int[][] mPath = new int[4][2];
 
     public Unlock(Context context) {
         super(context);
@@ -42,6 +50,11 @@ public class Unlock extends View implements View.OnTouchListener {
                 dotPos[i][j][2] = colors[0];
             }
         }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                touch2Pot[i][j] = 0f;
+            }
+        }
         setOnTouchListener(this);
     }
 
@@ -52,21 +65,10 @@ public class Unlock extends View implements View.OnTouchListener {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        drawSmallCircle(canvas, dotPos);
-//        drawBigCircle(canvas, dotPos);
-        linePoint.add(100f);
-        linePoint.add(100f);
-
-        linePoint.add(200f);
-        linePoint.add(200f);
-
-        linePoint.add(400f);
-        linePoint.add(700f);
-
-        linePoint.add(500f);
-        linePoint.add(200f);
+        drawSmallCircle(canvas, dotPos);
+        drawBigCircle(canvas, dotPos);
         drawLine(canvas, linePoint);
-
+        drawTouchLine(canvas, touch2Pot);
     }
 
     private void drawSmallCircle(Canvas canvas, float[][][] floats) {
@@ -104,6 +106,8 @@ public class Unlock extends View implements View.OnTouchListener {
                     setBigCircleColor(touched_pos);
                     linePoint.add(dotPos[touched_pos[0]][touched_pos[1]][0]);
                     linePoint.add(dotPos[touched_pos[0]][touched_pos[1]][1]);
+                    touch2Pot[0][0] = dotPos[touched_pos[0]][touched_pos[1]][0];
+                    touch2Pot[0][1] = dotPos[touched_pos[0]][touched_pos[1]][1];
                     invalidate();
                 }
                 break;
@@ -113,30 +117,47 @@ public class Unlock extends View implements View.OnTouchListener {
                     setBigCircleColor(touched_pos);
                     linePoint.add(dotPos[touched_pos[0]][touched_pos[1]][0]);
                     linePoint.add(dotPos[touched_pos[0]][touched_pos[1]][1]);
-                    invalidate();
+                    touch2Pot[0][0] = dotPos[touched_pos[0]][touched_pos[1]][0];
+                    touch2Pot[0][1] = dotPos[touched_pos[0]][touched_pos[1]][1];
+                } else {
+                    touch2Pot[1][0] = x;
+                    touch2Pot[1][1] = y;
                 }
+                invalidate();
                 break;
             case MotionEvent.ACTION_UP:
                 clearBigCircleColor();
                 linePoint.clear();
+                clearTouchline();
                 invalidate();
                 break;
         }
         return true;
     }
 
+    private void clearTouchline() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                touch2Pot[i][j] = 0f;
+            }
+        }
+    }
+
     private void drawLine(Canvas canvas, ArrayList<Float> arrayList_float) {
         int color = colors[2];
         paint.setColor(color);
-        paint.setStrokeWidth(3);
-        int size=arrayList_float.size();
+        paint.setStrokeWidth(10);
+        int size = arrayList_float.size();
+        if (size < 4) {
+            return;
+        }
         float[] line_point = new float[2 + (size - 4) * 2 + 2];
         Iterator iterator = arrayList_float.iterator();
         int i = 0;
         while (iterator.hasNext()) {
             float valueX = (float) iterator.next();
             float valueY = (float) iterator.next();
-            if (i == 0 || i==(size-3)*2) {
+            if (i == 0 || i == (size - 3) * 2) {
                 line_point[i++] = valueX;
                 line_point[i++] = valueY;
             } else {
@@ -148,6 +169,16 @@ public class Unlock extends View implements View.OnTouchListener {
 
         }
         canvas.drawLines(line_point, paint);
+    }
+
+    private void drawTouchLine(Canvas canvas, float[][] pot2) {
+
+        if (pot2[0][1] + pot2[0][0] < 1f || pot2[1][0] + pot2[1][1] < 1f) {
+            return;
+        }
+        paint.setColor(colors[2]);
+        paint.setStrokeWidth(10);
+        canvas.drawLine(pot2[0][0], pot2[0][1], pot2[1][0], pot2[1][1], paint);
     }
 
     public int[] getTouchDot(float x, float y) {
@@ -181,5 +212,20 @@ public class Unlock extends View implements View.OnTouchListener {
 
     private float getDistance(float x1, float y1, float x2, float y2) {
         return (float) Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
+    }
+
+    private boolean checkPath(int[][] path) {
+        int len = rightPath.length;
+        if (len != path.length) {
+            return false;
+        }
+        for (int i = 0; i < len; i++) {
+           for(int j=0;j<2;j++){
+               if(rightPath[i][j]!=path[i][j]){
+                   return false;
+               }
+           }
+        }
+        return true;
     }
 }
